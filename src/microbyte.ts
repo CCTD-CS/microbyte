0f5277a5bc722dab81a2b798199b84546fedbc5e
r59q <andreashenriksen@yahoo.dk>
2024-09-03 21:00:38 +0200

    Export microbit handler

--- src/microbyte.ts
+++ src/microbyte.ts
@@ -3,7 +3,8 @@ import { Microbit } from "./implementations/Microbit";
 import { MicrobitBluetoothDevice } from "./implementations/MicrobitBluetoothDevice";
 import { MicrobitDeviceState } from "./implementations/MicrobitDevice";
 import USBController from "./implementations/USBController";
+import { MicrobitHandler } from "./interfaces/MicrobitHandler";
 import { PairingPattern } from "./utils/PairingPattern";
 
 // Export Microbit base
-export { Microbit, PairingPattern, MicrobitBluetoothDevice, USBController, MicrobitDeviceState, MBSpecs };
+export { Microbit, PairingPattern, MicrobitBluetoothDevice, USBController, MicrobitDeviceState, MBSpecs, MicrobitHandler };
